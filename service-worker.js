adf317488154a390bcd1a5c0e7b04f22b18a5aac
Aniket Anil Mali <aniketa5497@gmail.com>
2025-11-16 19:44:32 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,32 @@
+const CACHE_NAME = "expiry-audit-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("expiry-cache").then((cache) => {
-      return cache.addAll(["./", "./index.html"]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
+  
